f900a7aa7ae0ee5e6ea5133822916af488f6b79b
shaheemMPM <shaheem.mpm@gmail.com>
2025-07-23 07:40:17 +0530

    Cleanups UI of add task at bottom form

--- src/components/AddTaskAtBottomForm/AddTaskAtBottomForm.tsx
+++ src/components/AddTaskAtBottomForm/AddTaskAtBottomForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./AddTaskAtBottomForm.css";
+import CloseIcon from "../ui/icons/Close";
 
 type Props = {
   onCreate: (title: string, position: "top" | "bottom") => void;
@@ -23,44 +24,44 @@ const AddTaskAtBottomForm = ({ onCreate, expanded, setExpanded }: Props) => {
     setTitle("");
   };
 
-  return (
-    <div className="add-task-bottom-container">
-      {!expanded ? (
-        <div className="add-task-wrapper">
-          <button
-            type="button"
-            className="add-task-button"
-            onClick={() => setExpanded(true)}
-          >
-            + ADD TASK
-          </button>
-        </div>
-      ) : (
-        <div className="add-task-bottom-form">
+  if (expanded) {
+    return (
+      <div className="add-task-bottom-form">
+        <button type="button" onClick={handleCancel} className="cancel-button">
+          <CloseIcon size={11} color="#666" /> CANCEL
+        </button>
+        <div className="input-container">
+          <div className="input-label">Title</div>
           <input
             type="text"
             placeholder="Enter task title*"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <div className="form-buttons">
-            <button
-              type="button"
-              onClick={handleCancel}
-              className="cancel-button"
-            >
-              Cancel
-            </button>
-            <button
-              type="button"
-              onClick={handleSubmit}
-              className="confirm-button"
-            >
-              Add
-            </button>
-          </div>
         </div>
-      )}
+        <div className="form-footer">
+          <p className="form-subtitle">Add a new task</p>
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="confirm-button"
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="add-task-wrapper">
+      <button
+        type="button"
+        className="add-task-button"
+        onClick={() => setExpanded(true)}
+      >
+        + ADD TASK
+      </button>
     </div>
   );
 };
---
+++ src/components/ui/icons/Close.tsx
@@ -0,0 +1,36 @@
+type CloseIconProps = {
+  size?: number | string;
+  color?: string;
+};
+
+const CloseIcon = ({ size = 16, color = "#000000" }: CloseIconProps) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-labelledby="closeIconTitle"
+    >
+      <title id="closeIconTitle">Close Icon</title>
+      {/* <path
+        d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
+        fill={color}
+      /> */}
+      <g id="Menu / Close_LG">
+        <path
+          id="Vector"
+          d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001"
+          stroke={color}
+          strokeWidth={4}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default CloseIcon;
